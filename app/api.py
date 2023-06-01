b1b664612ac691eadde22a6e066b45e3d75c1e60
Catalina Gajardo Olmedo <104330559+Cataaa0@users.noreply.github.com>
2023-06-01 11:45:13 -0400

    Functions for route categories
    The following functions were added and reorganized:
    -get_columns_data_types
    -get_all_categories_as_dict_array
    -get_all_properties_from_category_as_dict_array
    -get_all_subcategories_id_of_category_as_array
    -add_column_to_category
    -delete_category_column
    -create_category
    
    Comments were placed in the code for things that remain to be fixed.

--- app/api.py
+++ app/api.py
@@ -1,4 +1,5 @@
 __author__ = 'ivana'
+import re
 
 #-------------------------General Functions---------------------------
 """Function that returns the data type of a variable"""
@@ -17,8 +18,15 @@ def set_as_list(string):
     lst = string.split(";")
     return [ r for r in lst if r != '']
 
-#-------------------------Functions for Index---------------------------
+"""Function that delivers the data type"""
+def get_columns_data_types():
+    return ['varchar', 'number', 'text']
 
+"""Funtion to"""
+#def show_colums_category(): -----------------------------------------DEFINIR
+
+
+#-------------------------Functions for Index---------------------------
 #---Functions to display form to add paper---
 """Funtion to"""
 def get_paper_properties(db):
@@ -50,7 +58,7 @@ def get_data_from_category_by_cat_id(db, cat_id):
         dict_array.append({'name':row[1],'id':row[0]})
     return dict_array
 
-#---functions for adding paper---
+#---Functions for adding paper---
 """Funtion to"""
 def add_paper_using_dict_array(db, dict_array):
     cursor = db.connection.cursor()
@@ -253,7 +261,7 @@ def edit_paper_using_dict_array(db, paper_id,dict_array):
     # Commit changes in the database
     db.connection.commit()
 
-
+"""Funtion to"""
 def update_authors_to_paper(db, paper_id, authors):
     cursor = db.connection.cursor()
     # remove authors from paper
@@ -263,7 +271,127 @@ def update_authors_to_paper(db, paper_id, authors):
     # Commit changes in the database
     db.connection.commit()
 
+"""Funtion to"""
 def update_categories_data_from_dict_array(db, categories, paper_id):
     cursor = db.connection.cursor()
     cursor.execute("DELETE FROM paper_has_cont WHERE paper_id=%s",(paper_id))
     add_data_to_categories_from_dict_array(db, categories, paper_id)
+
+
+#-------------------------Functions for Categories---------------------------
+#---functions for displaying the page od Categories---
+"""Funtion to"""
+def get_all_categories_as_dict_array(db):
+    cursor = db.connection.cursor()
+    cursor.execute("SELECT id, name FROM categories")
+    category_dict_array = []
+    for row in cursor.fetchall():
+        category_dict_array.append({'id':row[0],'name':row[1]})
+    return category_dict_array
+
+#---Functions to display the information of a Category--- ----------------------------ARREGLAR CUANDO PONGA EL TIPO DE DATO
+"""Funtion to"""
+def get_all_properties_from_category_as_dict_array(db, cat_id):
+    cursor = db.connection.cursor()
+    dict_array = []
+    # get all columns from the category table and create properties
+    cursor.execute("show columns from categories")
+    for row in cursor.fetchall():
+        if row[0] == 'id' or row[0] == 'extra':
+            continue
+        # - add 'type' to the dictionary
+        type = parse_type(row[1])
+        # - set 'is_subcat' to false
+        dict_array.append({'name':row[0],'type':type,'is_subcat':False,'properties':[]})
+    cursor.execute("SELECT extra FROM categories WHERE id=%s",(cat_id))
+    extra=cursor.fetchall()
+    extra1= re.split(';',extra[0][0])
+    for i in extra1:
+        if i=='':
+            continue
+        dict_array.append({'name':i,'type':'varchar','is_subcat':False,'properties':[]})
+    # get all subcategories from category
+    subcats_id = get_all_subcategories_id_of_category_as_array(db, cat_id)
+    for subcat_id in subcats_id:
+        subcat_name = get_subcategory_name_from_id(db, subcat_id)
+        # - for each subcategory, get their columns and add it to their properties
+        properties_type = get_subcategory_properties_type_as_dict(db,subcat_id)
+        properties = [name for name in properties_type]
+        # - add 'type'='subcat' to the dictionary
+        type = 'subcat'
+        # - set 'is_subcat' to true
+        is_subcat = 'True'
+        cursor.execute("SELECT name FROM interaction WHERE id IN (SELECT int_id FROM int_cont WHERE cont_id=%s or cont_id=%s)",[subcat_id,subcat_id])
+        for row in cursor.fetchall():
+            interaction = row[0]
+            dict_array.append({'name':subcat_name,'id':subcat_id,'properties':properties, 'properties_type':properties_type,
+                           'type':type,'is_subcat':is_subcat,'interaction':interaction})
+    return dict_array
+
+"""Funtion to"""
+def get_all_subcategories_id_of_category_as_array(db, cat_id):
+    cursor = db.connection.cursor()
+    cursor.execute("SELECT DISTINCT cont_id FROM cat_cont WHERE cat_id=%s", [cat_id])
+    subcat_id_array = []
+    for row in cursor.fetchall():
+        subcat_id_array.append(row[0])
+    return subcat_id_array
+
+"""Funtion to""" 
+#---Functions to add column to category---
+"""Funtion to""" 
+def add_column_to_category(db, cat_id, col_name, col_data):
+    cursor = db.connection.cursor()
+    # add column to table
+    cursor.execute("SELECT extra FROM categories WHERE id=%s",(cat_id))
+    extra=str(cursor.fetchall()[0][0])
+    extra+=col_name+"$"+col_data+";"
+    cursor.execute("UPDATE categories SET extra=%s WHERE id=%s",(extra,cat_id))
+    # Commit changes in the database
+    db.connection.commit()
+
+"""Funtion to""" 
+def delete_category_column(db, cat_id,column_name):
+    cursor = db.connection.cursor()
+    cursor.execute("SELECT extra FROM categories WHERE id=%s",(cat_id))
+    extra=cursor.fetchall()
+    while re.search(r"\$[^$;]*;", extra):
+        texto = re.sub(r"\$[^$;]*;", ";", extra, count=1)
+    extra1=re.split(';',extra1[0][0])
+    extranew=""
+    n=0
+    for i in extra1:
+        if i==column_name:
+            break
+        n+=1
+    for i in extra1:
+        if i==column_name or i=="":
+            continue
+        else:
+            extranew+=i+";"
+    # delete column
+    cursor.execute("UPDATE categories SET extra=%s WHERE id=%s",(extranew,cat_id)) 
+    cursor.execute("SELECT cont_id WHERE cat_id=%s",(cat_id))   #----------------------------------------PROBAR CUANDO AGREGUE CONT
+    for cont_id in cursor.fetchall():
+        cursor.execute("SELECT extra FROM content WHERE id=%s",(cont_id))
+        extra=cursor.fetchall()
+        extra=re.split(';', extra[0][0])
+        extranew=""
+        x=0
+        for i in extra:
+            if x==n:
+                continue
+            else:
+                extranew+=i+";"
+            x+=1
+        cursor.execute("UPDATE content SET extra=%s WHERE id=%s",(extranew,cont_id))
+    # Commit changes in the database
+    db.connection.commit()
+
+
+#---Functions to add category---
+def create_category(db,name, description):
+    cursor = db.connection.cursor()
+    cursor.execute('''INSERT INTO categories (name, description, extra)
+                  VALUES (%s, %s, %s)''', (name, description, ""))
+    db.connection.commit()
