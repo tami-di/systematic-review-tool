2509171e63c68f7d9adce03fda54b3a4fae1cb17
tami-di <tamiduenas@gmail.com>
2024-01-23 17:11:44 -0300

    sort table in search

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,32 +1,102 @@
 var is_submit_button = false
-var show_results =  function(results){
-
-    if(! ('data' in results)){
-
-        return
+var show_results = function(results) {
+    if (!('data' in results)) {
+        return;
     }
-    headers = results.headers
-    data = results.data
+    headers = results.headers;
+    data = results.data;
     d3.select("#results-body")
-    .append("h3")
-    .text(data.length + " matches were found")
+        .append("h3")
+        .text(data.length + " matches were found");
     table = d3.select("#results-body")
-    .append("table")
-    .attr("class","table table-bordered table-striped table-hover table-responsive")
-    header_row = table.append("thead").append("tr")
-    for(i = 0; i < headers.length; i++){
+        .append("table")
+        .attr("class", "table table-bordered table-striped table-hover table-responsive");
+    header_row = table.append("thead").append("tr");
+
+    for (i = 0; i < headers.length; i++) {
         header_row.append("td")
-        .text(headers[i])
+            .text(headers[i])
+            .attr("onclick", "sortTable(" + i + ")")
+            .attr("title", "Tooltip text for header " + headers[i]); 
     }
-    tbody = table.append("tbody")
-    for(k = 0; k < data.length; k++){
-        row = tbody.append("tr")
-        for(i = 0; i < headers.length; i++){
+
+    tbody = table.append("tbody");
+    for (k = 0; k < data.length; k++) {
+        row = tbody.append("tr");
+        for (i = 0; i < headers.length; i++) {
             row.append("td")
-            .text(data[k][headers[i]])
+                .text(data[k][headers[i]]);
         }
     }
 }
+
+
+
+
+
+
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = d3.select("#results-body").select("table").node();
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("td")[n];
+            console.log(x);
+            y = rows[i + 1].getElementsByTagName("td")[n];
+            console.log(y);
+            /* Check if the two rows should switch place,
+            based on the direction, asc or desc: */
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
+
+
+  
+
+
+
 
 // Initialize search form
 var set_form = function(dict){
